f0a070bd9cb43a3f620c1ebfcb53af4ab13f649e
galoryzen <jlopezr@uninorte.edu.co>
2022-09-23 07:52:24 -0500

    first commit

---
+++ prueba.c
@@ -0,0 +1,13 @@
+main (void)
+{ int i;
+int j;
+char c;
+char cadena
+float z;
+z=14.9e-8;
+z=12.9!;
+cadena="Hola";
+scanf ("%d",i);
+i=i*2;
+printf ("El doble es %d",i);
+}
---
+++ test.sh
@@ -0,0 +1,4 @@
+rm -rf lex.yy.c a.out salida.txt
+flex lab.l
+gcc lex.yy.c
+./a.out prueba.c
