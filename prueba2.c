773cc68f56cca263d7414df1805831d5ac148163
galoryzen <rjlopezgr@gmail.com>
2022-10-25 10:24:28 -0500

    Fixing errors before syntactic analyzer

---
+++ prueba2.c
@@ -0,0 +1,38 @@
+// #include <stdio.h>
+
+int main() {
+    int a, b, md, c;
+    int d = 15;
+    float x,y,z;
+
+// Aquí comienza el programa    
+    a = 27;
+    1b = 3;  // ERROR por definición de variable
+    x = 45,9; // ERROR por formato de número real
+    y = 3.3;
+    c = a + 7b;  // ERROR por definición de variable
+    printf("valor de suma %d\n", c);
+    c == a - b;  // NO ERROR
+    printf("valor de resta %d\n", c);
+    z1 = a / b;  // NO ERROR
+    printf("valor de división  %.3f\n", z);
+
+/*Calculo del máximo divisor de un numero */    
+    md = 0;
+    int $i = 1;  // ERROR por definición de variable
+    while (i < a {
+        // printf("valor de %d\n", i);
+        if (a%i == 0) 
+           md =/ i; // NO ERROR
+        i = i + 1;
+    }
+    printf("el máximo divisor de %d es %d\n", a, md);
+
+// Suma de los números menores que otro
+    int s = 0;
+    for (int j=1;j<=d;j = j + 2) {
+        s = s + 2j;  // ERROR por definición de variable
+    }
+    printf("la suma de los menores que %d es %d\n", d, 7s); // ERROR por definición de variable
+    
+}
